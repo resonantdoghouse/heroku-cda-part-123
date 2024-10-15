e9dfef731edc7d2796a1396ccd701db45d51ec24
Jim <jim@advancebass.com>
2024-10-15 12:42:20 -0700

    testing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 
 console.log(PORT);
+console.log("process.env", process.env);
 
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome to my Express App</h1>`);
